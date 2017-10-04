b933a030c335d0341424718a0460bfd37c408955
Craig Davison <craig@davison.io>
2017-10-04 16:25:28 +0100

    Use IDE

--- letterOccurs.java
+++
@@ -1,9 +0,0 @@
-public class letterOccurs {
-    public static void main(String[] args) {
-        String applecake = "Java apple cake";
-
-        int results = applecake.length() - applecake.replace("a", "").length();
-
-        System.out.println(results);
-    }
-}
