e636fc5ea4563774fd439f75b6523e88effb4ae9
61651 <craig@davison.io>
2017-11-29 13:34:29 +0000

    Convert string to string builder

--- src/hashASCII.java
+++ src/hashASCII.java
@@ -1,9 +1,9 @@
 public class hashASCII {
     public static void main(String[] args){
         String input = "Hello world";
-        String hash = "";
+        StringBuilder hash = new StringBuilder();
         for(int i = 0 ; i < input.length(); i++) {
-            hash += (int)input.charAt(i);
+            hash.append((int) input.charAt(i));
         }
         System.out.println(hash);
     }
